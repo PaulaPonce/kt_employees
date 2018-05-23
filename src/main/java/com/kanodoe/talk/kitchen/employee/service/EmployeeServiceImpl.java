a4720ff6ffe3bfac034faf59bca412ecf1896d59
Alejandro Cornejo <mackno@gmail.com>
2018-05-23 14:58:26 -0400

    Develop init

--- src/main/java/com/kanodoe/talk/kitchen/employee/service/EmployeeServiceImpl.java
+++ src/main/java/com/kanodoe/talk/kitchen/employee/service/EmployeeServiceImpl.java
@@ -1,9 +1,7 @@
 package com.kanodoe.talk.kitchen.employee.service;
 
 import com.kanodoe.talk.kitchen.employee.dao.EmployeeDAO;
-import com.kanodoe.talk.kitchen.employee.model.commons.Count;
 import com.kanodoe.talk.kitchen.employee.model.Employee;
-import com.kanodoe.talk.kitchen.employee.model.commons.Page;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
@@ -23,51 +21,6 @@ public class EmployeeServiceImpl implements EmployeeService {
     public EmployeeService getEmployees (Handler<AsyncResult<List<Employee>>> resultHandler) {
 
         employeeDAO.getEmployees(resultHandler);
-        return this;
-    }
-
-    @Override
-    public EmployeeService getEmployeeById (Long id, Handler<AsyncResult<Employee>> resultHandler) {
-
-        employeeDAO.getEmployeById(id, resultHandler);
-        return this;
-    }
-
-    @Override
-    public EmployeeService postEmployee (Employee employee, Handler<AsyncResult<Void>> resultHandler) {
-
-        employeeDAO.postEmployee (employee, resultHandler);
-
-        return this;
-    }
-
-    @Override
-    public EmployeeService putEmployee (Employee employee, Handler<AsyncResult<Void>> resultHandler) {
-
-        employeeDAO.putEmployee (employee, resultHandler);
-        return this;
-    }
-
-    @Override
-    public EmployeeService deleteEmployee (String id, Handler<AsyncResult<Void>> resultHandler) {
-
-        employeeDAO.deleteEmployee (id, resultHandler);
-        return this;
-    }
-
-    @Override
-    public EmployeeService getEmployeesPaginated (Page page, Handler<AsyncResult<List<Employee>>> resultHandler) {
-
-        employeeDAO.getByPage(page.getPage(), page.getLimit(), resultHandler);
-
-        return this;
-    }
-
-    @Override
-    public EmployeeService getEmployeesCount (String isActive, Handler<AsyncResult<Count>> resultHandler) {
-
-        employeeDAO.getEmployeesCount(isActive, resultHandler);
-
         return this;
     }
 }
